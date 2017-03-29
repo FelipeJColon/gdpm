e43c526153bd7a996c100f5f28ef54916e742d65
Lucie Contamin <contamin.lucie@gmail.com>
2017-03-29 16:02:31 +0700

    correct R documentation

--- R/gdpm.R
+++ R/gdpm.R
@@ -1,6 +1,7 @@
 #' gdpm : Surveillance Data from the Vietnamese GDPM
 #'
-#' Monthly surveillance data aggregated by province by the Vietnamese General Department of Preventive Medicine (GDPM)
+#' Monthly surveillance data aggregated by province by the Vietnamese General
+#' Department of Preventive Medicine (GDPM)
 #'
 #' @docType package
 #' @name gdpm
