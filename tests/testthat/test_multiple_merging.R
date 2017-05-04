2142d8648ef7e0e31da0e785894eb259cf6ba7da
Lucie Contamin <contamin.lucie@gmail.com>
2017-05-04 14:11:57 +0700

    update getid function

--- tests/testthat/test_multiple_merging.R
+++ tests/testthat/test_multiple_merging.R
@@ -16,24 +16,14 @@ test_that("`getid` returns the correct time range", {
     c(1980, 2015))
 
   expect_equal(
-    range(getid(chickenpox, hepatitis, ili, dengue,
-      range_cut = "shortest")$year),
+    range(getid(chickenpox, hepatitis, ili, dengue, shortest = TRUE)$year),
     c(1980, 2010))
 
   expect_equal(
     range(getid_("chickenpox", "hepatitis", "malaria", "dengue",
-      range_cut = "shortest")$year),
+      shortest = TRUE)$year),
     c(2003, 2010))
 
-  expect_equal(
-    range(getid(chickenpox, rubella, ili, dengue,
-      range_cut = "longest")$year),
-    c(2006, 2015))
-
-  expect_equal(
-    range(getid_("chickenpox", "hepatitis", "ili", "dengue",
-      range_cut = "longest")$year),
-    c(1980, 2015))
 
   expect_equal(
     range(getid(chickenpox, shigella, ili, dengue,
@@ -78,7 +68,7 @@ test_that("`getid` returns the correct province name", {
 
   expect_equal(
     unique(getid(chickenpox, hepatitis, ili, dengue,
-      range_cut = "shortest")$province),
+      shortest = TRUE)$province),
     c("An Giang", "Ba Ria - Vung Tau", "Bac Thai", "Ben Tre",  "Binh Tri Thien",
       "Cao Bang", "Cuu Long", "Dack Lak", "Dong Nai", "Dong Thap",
       "Gia Lai - Kon Tum", "Ha Bac", "Ha Nam Ninh", "Ha Noi", "Ha Tuyen",
@@ -90,7 +80,7 @@ test_that("`getid` returns the correct province name", {
 
   expect_equal(
     unique(getid_("chickenpox", "hepatitis", "malaria", "dengue",
-      range_cut = "shortest")$province),
+      shortest = TRUE)$province),
     c("An Giang", "Ba Ria - Vung Tau", "Bac Giang", "Bac Kan", "Bac Lieu",
       "Bac Ninh", "Ben Tre",  "Binh Dinh", "Binh Duong", "Binh Phuoc",
       "Binh Thuan", "Ca Mau", "Can Tho", "Cao Bang", "Da Nang", "Dack Lak",
@@ -103,34 +93,6 @@ test_that("`getid` returns the correct province name", {
       "Tay Ninh", "Thai Binh", "Thai Nguyen", "Thanh Hoa", "Thua Thien - Hue",
       "Tien Giang", "Tra Vinh", "Tuyen Quang", "Vinh Long", "Vinh Phuc",
       "Yen Bai"))
-
-  expect_equal(
-    unique(getid(chickenpox, rubella, ili, dengue,
-      range_cut = "longest")$province),
-    c("An Giang", "Ba Ria - Vung Tau", "Bac Giang", "Bac Kan", "Bac Lieu",
-      "Bac Ninh", "Ben Tre",  "Binh Dinh", "Binh Duong", "Binh Phuoc",
-      "Binh Thuan", "Ca Mau", "Can Tho", "Cao Bang", "Da Nang", "Dak Lak",
-      "Dak Nong", "Dien Bien", "Dong Nai", "Dong Thap", "Gia Lai", "Ha Giang",
-      "Ha Nam", "Ha Noi", "Ha Tinh", "Hai Duong", "Hai Phong",
-      "Hau Giang", "Ho Chi Minh", "Hoa Binh", "Hung Yen", "Khanh Hoa",
-      "Kien Giang", "Kon Tum", "Lai Chau", "Lam Dong", "Lang Son", "Lao Cai",
-      "Long An", "Nam Dinh", "Nghe An", "Ninh Binh", "Ninh Thuan", "Phu Tho",
-      "Phu Yen", "Quang Binh", "Quang Nam", "Quang Ngai", "Quang Ninh",
-      "Quang Tri", "Soc Trang", "Son La", "Tay Ninh", "Thai Binh",
-      "Thai Nguyen", "Thanh Hoa", "Thua Thien - Hue", "Tien Giang", "Tra Vinh",
-      "Tuyen Quang", "Vinh Long", "Vinh Phuc", "Yen Bai"))
-
-  expect_equal(
-    unique(getid_("chickenpox", "hepatitis", "ili", "dengue",
-      range_cut = "longest")$province),
-    c("An Giang", "Ba Ria - Vung Tau", "Bac Thai", "Ben Tre",  "Binh Tri Thien",
-      "Cao Bang", "Cuu Long", "Dack Lak", "Dong Nai", "Dong Thap",
-      "Gia Lai - Kon Tum", "Ha Bac", "Ha Nam Ninh", "Ha Noi", "Ha Tuyen",
-      "Hai Hung", "Hai Phong", "Hau Giang", "Ho Chi Minh", "Hoang Lien Son",
-      "Kien Giang", "Lai Chau", "Lam Dong", "Lang Son", "Long An",  "Minh Hai",
-      "Nghe Tinh", "Nghia Binh", "Phu Khanh", "Quang Nam - Da Nang",
-      "Quang Ninh", "Son La", "Song Be", "Tay Ninh", "Thai Binh", "Thanh Hoa",
-      "Thuan Hai", "Tien Giang", "Vinh Phu"))
 
   expect_equal(
     unique(getid(chickenpox, ili, dengue, shigella,
@@ -163,12 +125,13 @@ test_that("`getid` returns the correct province name", {
 
 test_that("`getid` returns an error", {
 
-  expect_error(getid(chickenpox, ili, dengue, malaria, from = 1980, to = 2002))
+  expect_warning(getid(chickenpox, ili, dengue, malaria, from = 1980,
+    to = 2002))
 
   expect_error(getid(chickenpox, ili, dengue, malaria, from = 2028))
 
   expect_error(getid_("chickenpox","dengue","ili","vhf","malaria",
-    range_cut = "shortest"))
+    shortest = TRUE))
 
   expect_error(getid_("vhf","mumps", from = "1990", to = "1980"))
 
