13fbc75247e08e2a57a0524bc8aecc7aa61670cb
Lucie Contamin <contamin.lucie@gmail.com>
2017-06-01 16:08:30 +0700

    documentation functions

--- R/choropleth_map.R
+++ R/choropleth_map.R
@@ -1,54 +1,113 @@
-# System
-library(dplyr)
-library(magrittr)
-library(gdpm)
-
-# Function ####################################################################
-# Specific GDPM ----------------------------------------------------------------
 #' Makes a spatio-temporal choropleth map of a disease
 #'
 #' @param disease name of an epidemiological data frame (e.g. \code{ili}
 #' @param ye an year to indicate the temporal information
 #' @param month month to precise the temporal information
-#' @param x a value for the x coordinate of the top-left part of the legend
-#' @param y a value for the y coordinate of the top-left part of the legend
+#' @param x a value for the x coordinate of the top-left point of the legend
+#' @param y a value for the y coordinate of the top-left point of the legend
 #' @param sel a character value which can be "incidence" (by default) or
 #' "mortality" used to select the data expressed in the map
-#' @param col a vector of colors to use for the heatmap.
-#' @param style a character value isued from the \code{classint} package, and
+#' @param n a numeric indicating the number of intervals to represent the data
+#' (by default, \code{n = 6})
+#' @param col a vector of colors to use for the map (by default,
+#' \code{col = "YlOrBr"}.The colors from the package RColorBrewer can also be
+#' used.
+#' @param style a character value issued from the \code{classint} package, and
 #' used to select a method for the different way of calculating the intervals
+#' (by default \code{style = "quantile"})
 #' @param col_na the color with which to represent the missing values
+#' (by default \code{col_na = "grey"})
+#' @param fixedBreaks issued from the \code{classint} package. By default
+#' \code{NULL} but if a vector value is inputed, it will be used to specifen the
+#'  breaks
+#' @param locate if TRUE, call the function \code{locator} to indicate the
+#' top-left point of the legend
+#' @param pos by default \code{top-left}, but can be \code{top-right,
+#' bottom-left or bottom-right} can be used to indicate the position of the data
+#' if \code{x, y} are not indicated
+#' @param distrib if TRUE, print on the map, the distribution of the values by
+#' intervals
 #' @param h legend parameter expressing the height of one rectangle
-#' @param w legend parameter expressing the width of the rectangle
+#' in the legend
+#' @param w legend parameter expressing the width of the legend
 #' @param tl legend parameter expressing the length of the tick
 #' @param s legend parameter expressing the space between the text and the
 #' tick
-#' @param adj legend parameter expressing the alignment of the text, 1 (to the
-#' right) by default
-#' @param ... if need to imput more text parameter
-#'
+#' @param ... if need to imput more text parameters for the legend
 #'
 #' @examples
-#' # A heatmap of the ILI data:
-#' gdpm_choropleth("ili", 1980, x = 93, y = 18)
+#' library(gdpm)
+#' # A choroplet map of the ILI data:
+#' gdpm_choropleth("ili", 2004, "April")
+#'
 #' # with some data transformations in order to reflect better the contrasts:
-#' gdpm_choropleth("ili", 1980, x = 93, y = 18, n = 6, col = heat.colors(6),
+#' gdpm_choropleth("ili", 2004, "April", x = 93, y = 18, n = 6, col = "YlOrBr",
 #'      style = "jenks")
-#' # using some other color palettes, for examples the ones fromt the
-#' # RColorBrewer package:
+#'
+#' # using some other color palettes, for examples the ones from the
+#' # RColorBrewer package or the one in the color Palettes of R :
 #' library(RColorBrewer)
 #' # to see the available color palettes:
 #' display.brewer.all()
-#' gdpm_choropleth("ili", 1980, x = 93, y = 18, n = 6, col = "YlOrBr",
+#' gdpm_choropleth("ili", 2004, "April", x = 93, y = 18, n = 6, col = "YlOrBr",
 #'      style = "jenks")
+#' gdpm_choropleth("ili", 2004, "April", x = 93, y = 18, n = 6,
+#'  col = heat.colors(6), style = "jenks")
+#'
 #' # changing the color of the missing values:
-#' gdpm_choropleth("ili", 1980, x = 93, y = 18, n = 6, col = "YlOrBr",
-#'      style = "jenks", col_na = "chartreuse")
+#' gdpm_choropleth("ili", 1980, "April", col_na = "chartreuse")
+#'
+#' # changing the legend text parameters
+#' gdpm_choropleth("ili", 1980, "April", n = 6, col = "YlOrBr", style = "jenks",
+#'   cex = 0.5)
+#'
+# # Doesn't print the distribution of the value by intervals
+#' gdpm_choropleth("ili", 1980, "April", n = 6, distrib = FALSE,
+#'   col = heat.colors(6), style = "jenks")
+#'
+#' # By default, the legend is on the top left of the figure, but the position
+#' # can be easily change by using the parameters pos
+#'  # top left
+#' gdpm_choropleth("ili", 2004, "April", n = 6, col = heat.colors(6),
+#'   style = "jenks")
+#' gdpm_choropleth("ili", 2004, "April", n = 6, pos = "top-left",
+#'   col = heat.colors(6), style = "jenks")
+#' # top right
+#' gdpm_choropleth("ili", 2004, "April", n = 6, pos = "top-right",
+#'   col = heat.colors(6), style = "jenks", distrib = FALSE)
+#' # bottom left
+#' gdpm_choropleth("ili", 2004, "April", n = 6, pos = "bottom-left",
+#'   col = heat.colors(6), style = "jenks")
+#' # bottom right
+#' gdpm_choropleth("ili", 2004, "April", n = 6, pos = "bottom-right",
+#'   col = heat.colors(6), style = "jenks", distrib = FALSE)
+#'
+#' # By default, the function print the map of the incidence value for one
+#' # month of one year, but the mortality can also be printed.
+#' gdpm_choropleth("ili", 2004, "April", n = 6, sel = "incidence",
+#'   col = heat.colors(6), style = "jenks")
+#' gdpm_choropleth("ili", 2004, "January", n = 3, sel = "mortality",
+#'   col = heat.colors(3), style = "jenks")
+#'
+#' # The intervals used to expressed the value can be input directly as
+#' # parameters in the function via the parameters fixedBreaks.
+#' # The given breaks should be of length n+1.
+#' gdpm_choropleth("ili", 2004, "April", n = 3, col = heat.colors(3),
+#'   fixedBreaks = c(0, 5000, 10000, 15000))
+#'
+#' # Using the locator to choose where to print the legend
+#' \dontrun{
+#'  gdpm_choropleth("ili", 1980, "April", n = 6, locate = TRUE,
+#'   col = heat.colors(6), style = "jenks")
+#' gdpm_choropleth("ili", 1980, "April", n = 6, locate = TRUE, x = 95, y = 34,
+#'   col = heat.colors(6), style = "jenks")
+#' }
+#'
 #' @export
 gdpm_choropleth <- function(disease, ye, month, x, y, sel = "incidence", n = 6,
-  col = "YlOrBr", style = "quantile",  col_na = "grey",
+  col = "YlOrBr", style = "quantile", col_na = "grey", fixedBreaks = NULL,
   locate = FALSE, pos = "top-left", distrib = TRUE,
-  h = 0.75, w = 0.75, tl = .2, s = .2, adj = 1, ...)
+  h = 0.75, w = 0.75, tl = .2, s = .2, ...)
   {
   if (sel == "incidence"){off <- "mortality"}
   if (sel == "mortality"){off <- "incidence"}
@@ -60,23 +119,57 @@ gdpm_choropleth <- function(disease, ye, month, x, y, sel = "incidence", n = 6,
     rename_("value" = name)
   df <- df[which(df$month == month),] %>%
     select(-month)
-    #summarise(value = ifelse(mean(is.na(incidence)) == 1,
-     # sum(incidence), sum(incidence, na.rm = TRUE))) %>%
-    #ungroup
+
   # draw the choropleth map
-  idcm(df, ye, x, y, locate = locate, pos = pos,
+  idcm(df, ye, x, y, locate = locate, pos = pos, fixedBreaks = fixedBreaks,
     n = n, col = col, style = style, col_na = col_na, distrib = distrib,
-    legend = legend, h = h, w = w, tl = tl, s = s, adj = adj, ...)
+    h = h, w = w, tl = tl, s = s, ...)
 }
 
 # GENERIC ----------------------------------------------------------------------
 
-# draw a choropleth map
+#' Draws a spatio-temporal choropleth map of Vietnam
+#'
+#' @param df a data frame containing two columns : one containing the province
+#' name and another containing the value to represent
+#' @param ye an year in a numeric format (to select the map corresponding to
+#' the data)
+#' @param x a value for the x coordinate of the top-left part of the legend
+#' @param y a value for the y coordinate of the top-left part of the legend
+#' @param n a numeric indicating the number of intervals to represent the data
+#' (by default, \code{n = 6})
+#' @param col a vector of colors to use for the map (by default,
+#' \code{col = "YlOrBr"}).The colors from the package RColorBrewer can also be
+#' used.
+#' @param style a character value issued from the \code{classint} package, and
+#' used to select a method for the different way of calculating the intervals
+#' (by default \code{style = "quantile"})
+#' @param col_na the color with which to represent the missing values
+#' (by default \code{col_na = "grey"})
+#' @param fixedBreaks issued from the \code{classint} package. By default
+#' \code{NULL} but if a vector value is inputed, it will be used to specifen the
+#'  breaks
+#' @param locate if TRUE, call the function \code{locator} to indicate the
+#' top-left point of the legend
+#' @param pos by default \code{top-left}, but can be \code{top-right,
+#' bottom-left or bottom-right} can be used to indicate the position of the data
+#' if \code{x, y} are not indicated
+#' @param distrib if TRUE, print on the map, the distribution of the values by
+#' intervals
+#' @param h legend parameter expressing the height of one rectangle
+#' in the legend
+#' @param w legend parameter expressing the width of the legend
+#' @param tl legend parameter expressing the length of the tick
+#' @param s legend parameter expressing the space between the text and the
+#' tick
+#' @param ... if need to imput more text parameters for the legend
+#'
+#' @keywords internal
+#' @noRd
 idcm <- function(df, ye, x, y,
-  distrib = FALSE,
   n = 6, col = "YlOrBr", style = "quantile",  col_na = "grey",
-  legend, pos = "top-left",
-  locate= FALSE, h = 0.75, w = 0.75, tl = .2, s = .2, adj = 1, ...) {
+  fixedBreaks = NULL, locate = FALSE, pos = "top-left", distrib = TRUE,
+  h = 0.75, w = 0.75, tl = .2, s = .2, ...) {
 
   # graph parameters
   par <- par(fig = c(0,1,0,1), mar = c(5.1, 4.1, 4.1, 2.1))
@@ -84,7 +177,7 @@ idcm <- function(df, ye, x, y,
   # implement the incidence data in the shape file data
   require(gadmVN)
   provinces <- gadm(date = ye)
-  provinces <- merge(provinces, df)
+  provinces <- sp::merge(provinces, df)
 
   # choose class interval and color
   require(RColorBrewer)
@@ -94,18 +187,24 @@ idcm <- function(df, ye, x, y,
   } else {
     pal = brewer.pal(n, col)
   }
-  classint <- suppressWarnings(classIntervals(provinces$value, n = n,
-    style = style, na.rm = TRUE))
+   if(length(fixedBreaks) != 0){
+     classint <- suppressWarnings(classIntervals(provinces$value, n = n,
+       style = "fixed", fixedBreaks = fixedBreaks, na.rm = TRUE))
+   } else {
+     classint <- suppressWarnings(classIntervals(provinces$value, n = n,
+       style = style, na.rm = TRUE))
+   }
 
   # plot the result
   classint_colors <- findColours(classint, pal) %>%
     replace(is.na(.), col_na)
+  #return(provinces)
   plot(provinces, col = classint_colors)
 
-  # legend
-  wrap_legend(x, y, legend = classint$brks, col = pal, locate = locate,
+   legend
+  legend2(x, y, legend = classint$brks, col = pal, locate = locate,
     pos = pos, h = h, w = w, tl = tl,
-    s = s, adj = adj, ...)
+    s = s, ...)
 
   # if ask, plot the quantile distribution (bottom right)
   if (distrib == TRUE){
@@ -116,17 +215,37 @@ idcm <- function(df, ye, x, y,
     ytop <- plotdim[4] - (plotdim[4] - plotdim[3]) * 0.3
     par(fig = c(xleft, xright, ybottom, ytop), new = TRUE, mar = c(0,0,0,0))
 
-    quantile <- suppressWarnings(classIntervals(provinces$value, n = n ,
-      style = style, na.rm = TRUE))
+    if(length(fixedBreaks) != 0){
+      quantile <- suppressWarnings(classIntervals(provinces$value, n = n,
+        style = "fixed", fixedBreaks = fixedBreaks, na.rm = TRUE))
+    } else {
+      quantile <- suppressWarnings(classIntervals(provinces$value, n = n ,
+        style = style, na.rm = TRUE))
+    }
     plot(quantile, pal = pal, main = "")
   }
 
 }
 
 
-# add a legend to a plot
-legend2 <- function(x, y, legend, col = c("red", "green", "blue"),
-  h = 0.75, w = 0.75, tl = .2, s = .2, adj = 1, ...) {
+#' Draws a legend
+#'
+#' @param x a value for the x coordinate of the top-left part of the legend
+#' @param y a value for the y coordinate of the top-left part of the legend
+#' @param legend a character vector
+#' @param col a vector of colors
+#' @param h legend parameter expressing the height of one rectangle
+#' in the legend
+#' @param w legend parameter expressing the width of the legend
+#' @param tl legend parameter expressing the length of the tick
+#' @param s legend parameter expressing the space between the text and the
+#' tick
+#' @param ... if need to imput more text parameters for the legend
+#'
+#' @keywords internal
+#' @noRd
+square_legend <- function(x, y, legend, col,
+  h = 0.75, w = 0.75, tl = .2, s = .2, ...) {
 
   # size of the top character (width height)
   size_legend <- legend %>% as.character %>% tail(1) %>%
@@ -141,14 +260,33 @@ legend2 <- function(x, y, legend, col = c("red", "green", "blue"),
     rect(xleft, y[i + 1], xright, y[i], col = col[i], border = NA)
   rect(xleft, tail(y, 1), xright, y[1])
   segments(xleft, y, xleft - tl, y)
-  text(x + size_legend, y, rev(legend), adj = adj, ...)
+  text(x + size_legend, y, rev(legend), adj = 1, ...)
 
 }
 
-# Development -----------------------------------------------------------------
-
-wrap_legend <- function(x, y, legend, col, pos = "top-left", locate = FALSE,
-  h = 0.75, w = 0.75, tl = .2, s = .2, adj = 1, ...){
+#' Draws a legend
+#'
+#' @param x a value for the x coordinate of the top-left part of the legend
+#' @param y a value for the y coordinate of the top-left part of the legend
+#' @param legend a character vector
+#' @param col a vector of colors
+#' @param locate if TRUE, call the function \code{locator} to indicate the
+#' top-left point of the legend
+#' @param pos by default \code{top-left}, but can be \code{top-right,
+#' bottom-left or bottom-right} can be used to indicate the position of the data
+#' if \code{x, y} are not indicated
+#' @param h legend parameter expressing the height of one rectangle
+#' in the legend
+#' @param w legend parameter expressing the width of the legend
+#' @param tl legend parameter expressing the length of the tick
+#' @param s legend parameter expressing the space between the text and the
+#' tick
+#' @param ... if need to imput more text parameters for the legend
+#'
+#' @keywords internal
+#' @noRd
+legend2 <- function(x, y, legend, col, locate = FALSE, pos = "top-left",
+  h = 0.75, w = 0.75, tl = .2, s = .2, ...){
 
   if (missing(x) & missing(y) & locate == FALSE){
     usr <- par("usr")
@@ -179,82 +317,14 @@ wrap_legend <- function(x, y, legend, col, pos = "top-left", locate = FALSE,
 
   }
 
-  if (missing(x) & missing(y) & locate == TRUE){
+  if (locate == TRUE){
     coordinates <- locator(1)
     x = coordinates$x
     y = coordinates$y
   }
 
-  legend2(x, y, legend = legend, col = col , h = h, w = w, tl = tl, s = s,
-    adj = adj, ...)
+  square_legend(x, y, legend = legend, col = col , h = h, w = w, tl = tl,
+    s = s, ...)
 }
-
-# test -------------------------------------------------------------------------
-
-plot(1:10, 1:10)
-points(1, 9, col = "blue")
-points(2, 9, col ="pink" )
-#legend2(1,9, letters[1:3], col = heat.colors(2))
-legend2(1,9, rep("patate", 3), col = heat.colors(2))
-wrap_legend(legend = c("zero", "yellow", "red"), col = heat.colors(2))
-wrap_legend(locate = TRUE, legend = letters[1:3], col = heat.colors(2))
-wrap_legend(legend = letters[1:3], col = heat.colors(2))
-wrap_legend(legend = letters[1:3], col = heat.colors(2), pos = "top-right")
-
-# read the data
-gdpm_choropleth("ili", 1980, "January")
-gdpm_choropleth("ili", 1980, "April", n = 6, col = "YlOrBr", style = "jenks")
-
-# ... correspond to the legend text parameters
-gdpm_choropleth("ili", 1980, "April", n = 6, col = "YlOrBr", style = "jenks",
-  cex = 0.5)
-
-# test colors
-gdpm_choropleth("ili", 1980, "April", n = 6, col = heat.colors(6),
-  style = "jenks")
-
-# test styles
-gdpm_choropleth("ili", 1980, "April", n = 6, col = heat.colors(6),
-  style = "quantile")
-
-# test locator
-gdpm_choropleth("ili", 1980, "April", n = 6, locate = TRUE,
-  col = heat.colors(6), style = "jenks")
-
-# test distribution FALSE
-gdpm_choropleth("ili", 1980, "April", n = 6, distrib = FALSE,
-  col = heat.colors(6), style = "jenks")
-
-# test position
-# top left
-gdpm_choropleth("ili", 2004, "April", n = 6, col = heat.colors(6),
-  style = "jenks")
-gdpm_choropleth("ili", 2004, "April", n = 6, pos = "top-left",
-  col = heat.colors(6), style = "jenks")
-# top right
-gdpm_choropleth("ili", 2004, "April", n = 6, pos = "top-right",
-  col = heat.colors(6), style = "jenks", distrib = FALSE)
-# bottom left
-gdpm_choropleth("ili", 2004, "April", n = 6, pos = "bottom-left",
-  col = heat.colors(6), style = "jenks")
-# bottom right
-gdpm_choropleth("ili", 2004, "April", n = 6, pos = "bottom-right",
-  col = heat.colors(6), style = "jenks", distrib = FALSE)
-
-# test when x & y are inputed
-gdpm_choropleth("ili", 1980, "April", x = 110, y = 22, n = 6, col = "YlOrBr",
-  style = "jenks", col_na = "chartreuse", adj = 1)
-points(110,22, col = "red")
-
-# test color NA
-gdpm_choropleth("ili", 1980,  "April", n = 6, col = "YlOrBr",
-     style = "jenks", col_na = "chartreuse", adj = 1)
-
-# test sel parameters
-gdpm_choropleth("ili", 2004, "April", n = 6, sel = "incidence",
-  col = heat.colors(6), style = "jenks")
-gdpm_choropleth("ili", 2004, "January", n = 3, sel = "mortality",
-  col = heat.colors(3), style = "jenks")
-
 
 
