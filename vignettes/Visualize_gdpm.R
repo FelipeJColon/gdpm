ce5936f7e1c3e371f787007ca99a604cadcd9a7a
Lucie Contamin <contamin.lucie@gmail.com>
2017-11-16 17:06:01 +0700

    create visualization vignette

---
+++ vignettes/Visualize_gdpm.R
@@ -0,0 +1,194 @@
+## ----include=F-----------------------------------------------------------
+knitr::knit_hooks$set(margin = function(before,options,envir) {
+if(before) par(mgp=c(1,0.35,0),bty="n",plt=c(0,.99,.13,.99), mar = c(2,2,3,2), xpd = TRUE) else NULL })
+
+knitr::opts_chunk$set(margin=T,prompt=T,comment="",collapse=T,cache=F, bty="n",
+dev.args=list(pointsize=11),fig.height= 4,
+fig.width=6.24725,fig.retina=2,fig.align="center")
+
+## ----eval=F--------------------------------------------------------------
+#  devtools::install_github("choisy/gdpm")
+
+## ------------------------------------------------------------------------
+library(gdpm)
+
+## ------------------------------------------------------------------------
+dengue <- getid(dengue, from = 2000)
+head(dengue)
+
+## ----eval=F--------------------------------------------------------------
+#  devtools::install_github("choisy/poseid")
+
+## ------------------------------------------------------------------------
+library(poseid)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  ?poseid::sthm
+
+## ------------------------------------------------------------------------
+str(dengue)
+
+## ---- message=FALSE------------------------------------------------------
+# the dengue dataframe need to be transform before visualization and we need the `dplyr` package for that.
+#install.package("dplyr")
+library(dplyr)
+
+## ------------------------------------------------------------------------
+dengue <- mutate(dengue, time = as.Date(paste0(year, "-", as.numeric(month), "-", 15)))
+dengue <- select(dengue, matches("province"), matches("time"), contains("incidence"))
+dengue <- arrange(dengue, time)
+
+# We can check now that dengue is in a good format.
+str(dengue)
+
+## ------------------------------------------------------------------------
+sthm(dengue)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  ?legend2
+
+## ------------------------------------------------------------------------
+a <- sthm(dengue)
+# we used the parameter of sthm to complete the legend2 parameters, by default col = heat.colors(12), x = 0.85
+legend2(.9, 1, legend =  a, col = heat.colors(12), postext = "right", h = 1/length(heat.colors(12)), w = 0.04, tl = 0.01, s = 0.005)
+
+## ------------------------------------------------------------------------
+col <- rev(heat.colors(200))
+a <- sthm(dengue, col = col)
+# we used the parameter of sthm to complete the legend2 parameters
+legend2(.9, 1, legend =  a, col = col, postext = "right", h = 1/(length(a) -1), w = 0.04, tl = 0.01, s = 0.005)
+
+## ------------------------------------------------------------------------
+a <- sthm(dengue, f = sqrt, col = col)
+legend2(.9, 1, legend =  a, col = col, postext = "right", h = 1/(length(a) -1), w = 0.04, tl = 0.01, s = 0.005)
+a <- sthm(dengue, f = function(x) x^.3, col = col)
+legend2(.9, 1, legend =  a, col = col, postext = "right", h = 1/(length(a) -1), w = 0.04, tl = 0.01, s = 0.005)
+
+## ------------------------------------------------------------------------
+dengue <- getid(dengue)
+# data preparation
+dengue <- mutate(dengue, time = as.Date(paste0(year, "-", as.numeric(month), "-", 15)))
+dengue <- select(dengue, matches("province"), matches("time"), contains("incidence"))
+dengue <- arrange(dengue, time)
+
+str(dengue)
+
+
+## ------------------------------------------------------------------------
+# changing the color and size of the missing values:
+a <- sthm(dengue, f = function(x) x^.3, col = col)
+legend2(.9, 1, legend =  a, col =  col, postext = "right", col_na = "grey", size_na = 0.05, h = 1/(length(a) -1), w = 0.04, tl = 0.01, s = 0.005)
+a <- sthm(dengue, f = function(x) x^.3, col = col, col_na = "black")
+legend2(.9, 1, legend =  a, col =  col, postext = "right", col_na = "black", size_na = 0.05, h = 1/(length(a) -1), w = 0.04, tl = 0.01, s = 0.005)
+
+## ---- message = FALSE----------------------------------------------------
+library(gadmVN)
+library(sp)
+
+## ------------------------------------------------------------------------
+# extract the coordinates of each province
+provinces <- gadmVN::gadm(date = "1980-01-01", merge_hanoi = TRUE)
+coord <- sp::coordinates(provinces)
+row.names(coord) <- unique(provinces@data$province)
+head(coord)
+# ordered the provinces by latitude and create an index called "order"
+order <- rownames(coord[order(coord[, 2]), ])
+head(order)
+order <- data.frame(province = order, order = seq_along(order))
+head(order)
+
+## ------------------------------------------------------------------------
+dengue <- left_join(dengue, order, by = "province")
+dengue <-  arrange(dengue, order)
+head(dengue)
+
+## ------------------------------------------------------------------------
+dengue <- select(dengue, -order)
+str(dengue)
+a <- sthm(dengue, f = function(x) x^.3, col = col, show_legend = TRUE)
+# a is a list containing both the legend and the province. To print the scale legend with legend2, it is important to specify 'a$legend'
+legend2(.9, 1, legend =  a$legend, col = col, postext = "right", h = 1/(length(a$legend) -1), w = 0.04, tl = 0.01, s = 0.005)
+str(a)
+
+## ------------------------------------------------------------------------
+a <- sthm(dengue, f = function(x) x^.3, col = col, map = provinces, xm = 0.25, show_legend = TRUE)
+# a is a list containing both the legend and the province. To print the scale legend with legend2, it is important to specify 'a$legend'
+legend2(.9, 1, legend =  a$legend, col = col, postext = "right", h = 1/(length(a$legend) -1), w = 0.04, tl = 0.01, s = 0.005)
+str(a)
+
+## ------------------------------------------------------------------------
+#install.package("magrittr")
+library(magrittr)
+
+## ------------------------------------------------------------------------
+# geographic data
+map <- gadmVN::gadm(date = 1980, merge_hanoi = TRUE)
+# color vector
+col <- rev(heat.colors(100))
+
+getid(dengue) %>%
+  dplyr::mutate(time = as.Date(
+    paste0(year, "-", as.numeric(month), "-", 15))) %>%
+  dplyr::select(province, time, contains("incidence")) %>%
+  dplyr::arrange(time) %>%
+  sthm(f = function(x) x^.3, col = col, xm = 0.25) %>%
+  legend2(.9, 1, legend =  ., col = col, postext = "right",
+          h = 1/(length(.)-1), w = 0.04, tl = 0.01, s = 0.005)
+
+## ---- message = FALSE----------------------------------------------------
+library(dplyr) # for "filter" and "select"
+
+## ---- eval = FALSE-------------------------------------------------------
+#  ?choromap
+
+## ------------------------------------------------------------------------
+# geographic data
+map <- gadmVN::gadm(date = 1992, merge_hanoi = TRUE)
+
+## ------------------------------------------------------------------------
+# Preparation data spatial and epidemiologic
+# dengue data
+dengue <- getid(dengue, from = 1992, to = 2010)
+dengue_0993  <- filter(dengue, year == 1993, month == "September")
+dengue_0993 <- select(dengue_0993, province, contains("incidence"))
+
+
+## ------------------------------------------------------------------------
+# ?poseid::breaks
+
+## ------------------------------------------------------------------------
+# plot Quantile
+q_breaks <- poseid::breaks(dengue_0993, "incidence_dengue", pal = rev(heat.colors(6)), distribution = TRUE)
+attr(q_breaks, "breaks")
+
+## ------------------------------------------------------------------------
+# plot Fisher - Jenkins
+f_breaks <- poseid::breaks(dengue_0993, "incidence_dengue", style = "fisher" ,pal = rev(heat.colors(6)), distribution = TRUE)
+attr(f_breaks, "breaks")
+
+## ------------------------------------------------------------------------
+a <- choromap(dengue_0993, map, fixedBreaks = c(0, 75, 200, 490, 720, 1100, 1300), col = rev(heat.colors(6)), col_na = "grey")
+# return invisibly the information for the legend (the value and the colors associated in a vector with attribut)
+legend2(legend = a, col = attr(a, "colors"), col_na = "grey")
+# By default, the legend is on the top-left part of the figure, for more information: `?legend2`
+
+## ------------------------------------------------------------------------
+getid(dengue, from = 1990, to = 2010) %>%
+  filter(year == 1993, month == "September") %>%
+  select(province, contains("incidence")) %>%
+  breaks("incidence_dengue",n = 6, style = "fisher",
+         pal = rev(heat.colors(6))) %>%
+  choromap(., map, fixedBreaks = attr(., "breaks"),
+           col = rev(heat.colors(6)), col_na = "gray") %>%
+  legend2(legend = ., col = attr(., "colors"), col_na = "gray")
+
+## ------------------------------------------------------------------------
+getid(dengue, from = 1990, to = 2010) %>%
+  filter(year == 1993, month == "September") %>%
+  select(province, contains("incidence")) %>%
+  breaks("incidence_dengue",n = 6, style = "fisher",
+         pal = rev(heat.colors(6)), distribution = TRUE) %>%
+  choromap(., map, fixedBreaks = attr(., "breaks"),
+           col = rev(heat.colors(6)), col_na = "gray") %>%
+  legend2(legend = ., col = attr(., "colors"), col_na = "gray")
+
